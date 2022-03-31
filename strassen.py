c8ae596d5d1a7315d4c6b1c56a69a3ab71438880
scarf005 <greenscarf005@gmail.com>
2022-03-31 21:34:50 +0900

    refactor: :recycle: refactor using matrix

--- strassen.py
+++ strassen.py
@@ -55,7 +55,9 @@ class Matrix:
         new = Matrix.empty(self.dim)
         for i in range(self.dim):
             for j in range(self.dim):
-                new[i, j] = sum(self[i, k] * other[k, j] for k in range(self.dim))
+                new[i, j] = sum(
+                    self[i, k] * other[k, j] for k in range(self.dim)
+                )
         return new
 
 
@@ -63,50 +65,58 @@ def create_array2d(dimension: int) -> Array2d:
     return [[0] * dimension for _ in range(dimension)]
 
 
-def strassen(A: Array2d, B: Array2d) -> Array2d:
-    n = len(A)
+def strassen(A: Matrix, B: Matrix) -> Matrix:
+    n = A.dim
     if n <= threshold:
-        return matrixmult(A, B)
+        return A @ B
     A11, A12, A21, A22 = divide(A)
     B11, B12, B21, B22 = divide(B)
-    M1 = strassen(madd(A11, A22), madd(B11, B22))
-    M2 = strassen(madd(A21, A22), B11)
-    M3 = strassen(A11, msub(B12, B22))
-    M4 = strassen(A22, msub(B21, B11))
-    M5 = strassen(madd(A11, A12), B22)
-    M6 = strassen(msub(A21, A11), madd(B11, B12))
-    M7 = strassen(msub(A12, A22), madd(B21, B22))
+    M1 = strassen(A11 + A22, B11 + B22)
+    M2 = strassen(A21 + A22, B11)
+    M3 = strassen(A11, B12 - B22)
+    M4 = strassen(A22, B21 - B11)
+    M5 = strassen(A11 + A12, B22)
+    M6 = strassen(A21 - A11, B11 + B12)
+    M7 = strassen(A12 - A22, B21 + B22)
     return conquer(M1, M2, M3, M4, M5, M6, M7)
 
 
-def divide(A) -> Tuple[Array2d, Array2d, Array2d, Array2d]:
-    n = len(A)
+def divide(A: Matrix) -> Tuple[Matrix, Matrix, Matrix, Matrix]:
+    n = A.dim
     m = n // 2
-    A11, A12, A21, A22 = [create_array2d(m) for _ in range(4)]
+    A11, A12, A21, A22 = [Matrix.empty(m) for _ in range(4)]
     for i in range(m):
         for j in range(m):
-            A11[i][j] = A[i][j]
-            A12[i][j] = A[i][j + m]
-            A21[i][j] = A[i + m][j]
-            A22[i][j] = A[i + m][j + m]
+            A11[i, j] = A[i, j]
+            A12[i, j] = A[i, j + m]
+            A21[i, j] = A[i + m, j]
+            A22[i, j] = A[i + m, j + m]
     return A11, A12, A21, A22
 
 
-def conquer(M1, M2, M3, M4, M5, M6, M7) -> Array2d:
-    C11 = madd(msub(madd(M1, M4), M5), M7)
-    C12 = madd(M3, M5)
-    C21 = madd(M2, M4)
-    C22 = madd(msub(madd(M1, M3), M2), M6)
-    m = len(C11)
+def conquer(
+    M1: Matrix,
+    M2: Matrix,
+    M3: Matrix,
+    M4: Matrix,
+    M5: Matrix,
+    M6: Matrix,
+    M7: Matrix,
+) -> Matrix:
+    C11 = M1 + M4 - M5 + M7
+    C12 = M3 + M5
+    C21 = M2 + M4
+    C22 = M1 + M3 - M2 + M6
+    m = C11.dim
     n = 2 * m
     # print(2 * m, 2 * m)
-    C = create_array2d(n)
+    C = Matrix.empty(n)
     for i in range(m):
         for j in range(m):
-            C[i][j] = C11[i][j]
-            C[i][j + m] = C12[i][j]
-            C[i + m][j] = C21[i][j]
-            C[i + m][j + m] = C22[i][j]
+            C[i, j] = C11[i, j]
+            C[i, j + m] = C12[i, j]
+            C[i + m, j] = C21[i, j]
+            C[i + m, j + m] = C22[i, j]
     return C
 
 
@@ -138,34 +148,19 @@ def matrixmult(A: Array2d, B: Array2d) -> Array2d:  # 1.4 Matrix Multiplication
     return C
 
 
-A = [
+A = Matrix([
     [1, 2, 3, 4],
     [5, 6, 7, 8],
     [9, 1, 2, 3],
     [4, 5, 6, 7],
-]
-B = [
+])
+B = Matrix([
     [8, 9, 1, 2],
     [3, 4, 5, 6],
     [7, 8, 9, 1],
     [2, 3, 4, 5],
-]
+])
 
 threshold = 2
 print(f"{threshold =}")
-pprint(A, width=40)
-pprint(B, width=40)
-pprint(madd(A, B), width=40)
-pprint(msub(A, B), width=40)
-pprint(strassen(A, B), width=40)
-print("===")
-Amat = Matrix(A)
-Bmat = Matrix(B)
-print(Amat)
-print(Bmat)
-print(Amat + Bmat)
-print(Amat - Bmat)
-print(Amat @ Bmat)
-# C = strassen(A, B)
-# for i, elem in enumerate(C):
-#     print(f"C[{i}] = {elem}")
+print(strassen(A, B))
