f931ccaad8a7a836c5a84013ada79621dc1cecaa
scarf <greenscarf005@gmail.com>
2022-04-04 23:27:53 +0900

    docs: :memo: readme auto-generation

--- README.md
+++ README.md
@@ -1,4 +1,23 @@
-# strassen
 
-series of refactoring for strassen algorithm.
-check each commit for more information.
+# Strassen
+
+스트라센 행렬곱 알고리즘 리팩토링
+
+## 목차
+|장|요약|
+|--|---|
+|01|[style: :bulb: add type hint](./doc/01.md)|
+|02|[refactor: :recycle: create a function that creates empty 2d array](./doc/02.md)|
+|03|[refactor: :recycle: use fstring](./doc/03.md)|
+|04|[refactor: :recycle: use matrix map](./doc/04.md)|
+|05|[feat: :sparkles: add matrix class](./doc/05.md)|
+|06|[feat: :sparkles: getter/setter, create empty matrix](./doc/06.md)|
+|07|[feat: :sparkles: matrix add, sub, mul](./doc/07.md)|
+|08|[refactor: :recycle: refactor using matrix](./doc/08.md)|
+|09|[refactor: :fire: remove unused function](./doc/09.md)|
+|10|[chore: :memo: add original file](./doc/10.md)|
+|11|[fix: :bug: only run on explicit run](./doc/11.md)|
+|12|[feat: :sparkles: implement slice](./doc/12.md)|
+|13|[refactor: :recycle: use slicing for divide](./doc/13.md)|
+|14|[refactor: :recycle: use tuple instead](./doc/14.md)|
+|15|[refactor: :recycle: use iteration instead](./doc/15.md)|
--- create_doc.py
+++ create_doc.py
@@ -5,6 +5,8 @@ from subprocess import run
 from textwrap import dedent
 from typing import Dict, Generator, Iterable, List, Tuple, TypeVar
 
+T = TypeVar("T")
+
 
 def get_git_hashes(rng: slice) -> Dict[str, str]:
     result = run(
@@ -26,9 +28,6 @@ def get_diffs(git_hash: str) -> List[str]:
     return re.split("(@@.*\n)", raw)[1:]
 
 
-T = TypeVar("T")
-
-
 def grouped(iterable: Iterable[T], n=2) -> Iterable[Tuple[T, ...]]:
     """s -> (s0,s1,s2,...sn-1), (sn,sn+1,sn+2,...s2n-1), ..."""
     iterators = [iter(iterable)]
@@ -45,12 +44,6 @@ def format_diff(diff_text: List[str]) -> Generator[str, None, None]:
 ```
         """
         )
-
-
-git_hashes = get_git_hashes(slice(1, 16))
-
-
-DOC_PATH = Path(__file__).parent / "doc"
 
 
 def create_text(name: int, git: str, oneline: str, diff: List[str]) -> str:
@@ -70,15 +63,48 @@ def create_text(name: int, git: str, oneline: str, diff: List[str]) -> str:
 ## 무엇을 바꿀까?
 {diffs}
 
-    """
+"""
 
 
-for name, git_hash in zip(range(1, 16), git_hashes.items()):
-    git, oneline = git_hash
-    path = Path(DOC_PATH / f"{name:02}.md")
-    diff = get_diffs(git)
-    text = create_text(name, git, oneline, diff)
-    path.write_text(text)
-    # pprint(diff)
-    # print(path)
-    # print(text)
+git_hashes = get_git_hashes(slice(1, 16))
+entries = zip(range(1, 16), git_hashes.keys(), git_hashes.values())
+DOC_PATH = Path(__file__).parent / "doc"
+
+
+def genearate_docs():
+    for name, git, oneline in entries:
+        path = Path(DOC_PATH / f"{name:02}.md")
+        diff = get_diffs(git)
+        text = create_text(name, git, oneline, diff)
+        path.write_text(text)
+        # pprint(diff)
+        # print(path)
+        # print(text)
+
+
+def generate_readme():
+    arr = [
+        f"|{name:02}|[{oneline}](./doc/{name:02}.md)|"
+        for name, _, oneline in entries
+    ]
+    contents = "\n".join(arr)
+    body = f"""
+# Strassen
+
+스트라센 행렬곱 알고리즘 리팩토링
+
+## 목차
+|장|요약|
+|--|---|
+{contents}
+"""
+
+    path = Path("README.md")
+    path.write_text(body)
+
+
+if __name__ == "__main__":
+    # genearate_docs()
+    generate_readme()
+
+# TODO: create diff-documentation generator tool?
